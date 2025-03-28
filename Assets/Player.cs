0116ea427969898803c4fab2c50fe51c34cf1adb
hanson67 <shinouneho2004d@gmail.com>
2025-03-28 18:01:07 +0900

    test2

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,9 +4,38 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
+    bool hide;
+
+
     void Move()
     {
-        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 0) * speed * Time.deltaTime;
+    }
+    float hide_elasped;
+    void Hide()
+    {
+        if (!hide & Input.GetAxisRaw("Vertical") < 0)
+        {
+            hide_elasped += Time.deltaTime;
+            if (hide_elasped > 1)
+            {
+                Debug.Log(0);
+                transform.GetComponent<SpriteRenderer>().enabled = false;
+                hide = true;
+            }
+
+        }
+        else if(Input.GetAxisRaw("Vertical") == 0)
+        {
+            hide_elasped = 0;
+        }
+        else
+        {
+            hide_elasped = 0;
+            transform.GetComponent<SpriteRenderer>().enabled = true;
+            hide = false;
+        }
     }
     // Start is called before the first frame update
     void Start()
@@ -17,6 +46,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        if(!hide)
         Move();
+    }
+    void Update()
+    {
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log(collision.tag);
+        if (collision.gameObject.tag == "hiding") Hide();
     }
 }
